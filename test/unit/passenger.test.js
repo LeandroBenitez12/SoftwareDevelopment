f4bbc3df1f43a23da975af1854afe06b5bea7a74
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2024-06-13 22:52:33 -0300

    BOOOOOOOOOOOOOOOOOOOOOOOOCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- test/unit/passenger.test.js
+++ test/unit/passenger.test.js
@@ -1,5 +1,7 @@
 import { Passenger } from "../../src/entities/passenger";
 import { Flight } from "../../src/entities/flight";
+import { City } from "../../src/entities/city";
+import { Airport } from "../../src/entities/airport";
 
 test("Passenger fly 2 times", () => {
   const passengerOne = new Passenger("keandro", "benitez");
@@ -13,4 +15,21 @@ test("Passenger fly 2 times", () => {
   let flightPassengerOne = passengerOne.countFlights();
 
   expect(flightPassengerOne).toBe(2);
+});
+
+test("Leandro visito 2 veces Parauai", () => {
+  //passenger
+  const airPortInParauai = new Airport();
+  const passOne = new Passenger("Leandro", "Benitez");
+  const flightToParauai = new Flight();
+  const flightToNose = new Flight();
+  passOne.addFlight(flightToParauai);
+  passOne.addFlight(flightToNose);
+  flightToParauai.setDepartureAirport(airPortInParauai);
+  flightToNose.setDepartureAirport(airPortInParauai);
+
+  const parauai = new City("Parauai");
+  parauai.addAirport(airPortInParauai);
+
+  expect(passOne.cantVecesQueVisitaste(parauai)).toBe(2);
 });
